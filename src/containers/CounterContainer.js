876ec447f8b0b105ca007db10d50627e0dd79a84
universe-ys <smiletmf@naver.com>
2022-08-29 11:55:01 +0900

    redux상태관리

---
+++ src/containers/CounterContainer.js
@@ -0,0 +1,57 @@
+import { useCallback } from 'react';
+import { connect, useDispatch, useSelector } from 'react-redux';
+// import { bindActionCreators } from 'redux';
+import Counter from '../components/Counter';
+import { increase, decrease } from '../modules/counter';
+
+// const CounterContainer = ({ number, increase, decrease }) => {
+//   return (
+//     <Counter number={number} onIncrease={increase} onDecrease={decrease} />
+//   );
+// };
+
+const CounterContainer = () => {
+  const number = useSelector((state) => state.counter.number);
+  const dispatch = useDispatch();
+  const onIncrease = useCallback(() => dispatch(increase()), [dispatch]);
+  const onDecrease = useCallback(() => dispatch(decrease()), [dispatch]);
+
+  return (
+    <Counter number={number} onIncrease={onIncrease} onDecrease={onDecrease} />
+  );
+};
+
+// const mapStateToProps = (state) => ({
+//   number: state.counter.number,
+// });
+// const mapDispatchToProps = (dispatch) => ({
+//   increase: () => {
+//     dispatch(increase());
+//   },
+//   decrease: () => {
+//     dispatch(decrease());
+//   },
+// });
+
+// export default connect(mapStateToProps, mapDispatchToProps)(CounterContainer);
+// export default connect(
+//   (state) => ({ number: state.counter.number }),
+//   (dispatch) => ({
+//     increase: () => dispatch(increase()),
+//     decrease: () => dispatch(decrease()),
+//   }),
+// )(CounterContainer);
+
+// export default connect(
+//   (state) => ({ number: state.counter.number }),
+//   (dispatch) =>
+//     bindActionCreators(
+//       {
+//         increase,
+//         decrease,
+//       },
+//       dispatch,
+//     ),
+// )(CounterContainer);
+
+export default CounterContainer;
--- src/index.js
+++ src/index.js
@@ -3,12 +3,20 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createStore } from 'redux';
+import rootReducer from './modules';
+import { Provider } from 'react-redux';
+import { devToolsEnhancer } from 'redux-devtools-extension';
+
+const store = createStore(rootReducer, devToolsEnhancer());
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
